c425610ea1feefe7aca889e9f7c45cb44bb45d34
yingrui <yingrui.f@gmail.com>
2024-09-22 16:08:55 +0800

    yingrui: add first app to options page

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,14 +1,40 @@
-import { Button, Form, Input, Switch, Modal, Tabs } from "antd";
-import { isEqual } from "lodash";
+import React, { useState } from "react";
+import { Layout, Menu, theme } from "antd";
+import type { MenuProps } from "antd";
+
 import withSuspense from "@src/shared/hoc/withSuspense";
 import withErrorBoundary from "@src/shared/hoc/withErrorBoundary";
-import configureStorage, {
-  DEFAULT_GM_CONFIG_VALUE,
-} from "@root/src/shared/storages/gluonConfig";
-import useStorage from "@root/src/shared/hooks/useStorage";
+import WriterApp from "@pages/options/writer/components/WriterApp/WriterApp";
+import "@pages/options/Options.css";
 
-const Options = () => {
-  return <div>Options Page</div>;
+const { Header, Content, Sider } = Layout;
+
+const header_items: MenuProps["items"] = [
+  { key: 1, label: "Writing" },
+  { key: 2, label: "MORE COMING SOON" },
+];
+
+const Options: React.FC = () => {
+  return (
+    <Layout>
+      <Header id="app-header">
+        <div className="demo-logo">
+          <img src="/icons/gm_logo.png" />
+          <h6>Guru Mason</h6>
+        </div>
+        <div className="nav-menus">
+          <Menu
+            theme="light"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={header_items}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </div>
+      </Header>
+      <WriterApp />
+    </Layout>
+  );
 };
 
 export default withErrorBoundary(
